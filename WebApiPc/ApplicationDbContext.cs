08ce52ae71819be157f6d6a021aa5cf2679ca9bb
Alan <alan_darkstar@hotmail.com>
2022-09-22 17:11:56 -0500

    Se subio el programa a github

---
+++ WebApiPc/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPc.Entidades;
+
+namespace WebApiPc
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Computadora> Computadoras { get; set; }
+
+    }
+}
---
+++ WebApiPc/Controllers/PcController.cs
@@ -0,0 +1,64 @@
+﻿using WebApiPc.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPc.Controllers
+{
+    [ApiController] 
+    [Route("computadora")] //es la ruta del controlador
+    public class PcController: ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public PcController(ApplicationDbContext dbcontext)
+        {
+            this.dbContext = dbcontext;
+        }
+
+        [HttpGet] 
+        public async Task<List<Computadora>> Get()
+        {
+                return await dbContext.Computadoras.ToListAsync();     
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Post(Computadora computadora)
+        {
+            dbContext.Add(computadora);
+            await dbContext.SaveChangesAsync(); 
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+
+        public async Task<ActionResult> Put(Computadora computadora, int id)
+        {
+            if(computadora.Id != id) 
+            {
+                return BadRequest("El id no coincide.");
+            }
+
+            dbContext.Update(computadora);
+            await dbContext.SaveChangesAsync(); 
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var exist = await dbContext.Computadoras.AnyAsync(x => x.Id == id);
+            if(!exist)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(new Computadora()
+            {
+                Id = id
+            });
+            await dbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
